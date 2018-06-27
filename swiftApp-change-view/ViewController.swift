0ec412a64619211975bb87a8efbcbad2ba40a77c
Sean_0504 <38977968+Sean-0504@users.noreply.github.com>
2018-06-27 14:49:17 +0900

    create first_view

---
+++ swiftApp-change-view/ViewController.swift
@@ -0,0 +1,15 @@
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    @IBAction func buttonTapped(_ sender: Any) {
+        print("hoge")
+    }
+}
